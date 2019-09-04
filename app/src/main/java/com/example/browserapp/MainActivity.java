63e6a9ebccab543fbe411355ea02e5d117ec3ceb
RAYMOND UYA <meals555i@gmail.com>
2019-09-04 13:56:21 -0700

    Browse on that Page

--- app/src/main/java/com/example/browserapp/MainActivity.java
+++ app/src/main/java/com/example/browserapp/MainActivity.java
@@ -5,19 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button browser_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public  void Browser (View vv) {
+        browser_btn = findViewById(R.id.start_browser);
 
-        Intent intent = new Intent(this, myBrowser.class);
-        startActivity(intent);
+        browser_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                Intent intent = new Intent(MainActivity.this, myBrowser.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/browserapp/myBrowser.java
+++ app/src/main/java/com/example/browserapp/myBrowser.java
@@ -3,7 +3,9 @@ package com.example.browserapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class myBrowser extends AppCompatActivity {
 
@@ -15,8 +17,30 @@ public class myBrowser extends AppCompatActivity {
         setContentView(R.layout.activity_my_browser);
 
         web = findViewById(R.id.webView);
+
         web.getSettings().setLoadsImagesAutomatically(true);
+
+        WebSettings webSettings = web.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+
         web.loadUrl("https://www.google.co.in");
+
+        web.setWebViewClient(new WebViewClient());
+
     }
 
+    @Override
+    public void onBackPressed() {
+
+        if (web.canGoBack()) {
+
+            web.goBack();
+
+        } else {
+
+            super.onBackPressed();
+
+        }
+    }
 }
